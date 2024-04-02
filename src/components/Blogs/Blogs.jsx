5952bbd306005a99bdcf091889398493c9ea5ade
Rajeev Kumar <46224203+rkcoola792@users.noreply.github.com>
2024-04-02 09:46:26 +0530

    blogs page done

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -3,19 +3,20 @@ import Hero from '../Service/Hero'
 import Section2 from './Section2';
 import Section3 from '../Service/Section3';
 import Section4 from './Section4';
+import Section6 from '../Homepage/Section6';
 
 
 
 const Blogs = () => {
   return (
-    <div className="blogs px-12">
+    <div className="blogs px-4 sm:px-12">
       <Hero
         heading1="home"
         heading2="blog"
         subheading="our latest blogs"
         description="We’ve been told it is possible to revolutionize the payment industry. We have not reinvented the wheel, we decided to build upon it - successfully."
       ></Hero>
-      <Section2></Section2>
+      <Section6></Section6>
 
       <div className='my-24 bg-subtleBlue'>
 
--- src/components/Blogs/Section2.jsx
+++ src/components/Blogs/Section2.jsx
@@ -5,9 +5,9 @@ import BlogsPage from './BlogsPage';
 
 const Section2 = () => {
   return (
-    <div className="section2 bg-subtleBlue  flex flex-col">
-      <div className="top flex justify-between items-center">
-        <div className="left flex gap-6">
+    <div className="section2 bg-subtleBlue  flex flex-col gap-8">
+      <div className="top sm:flex-row flex flex-col gap-4 justify-between items-center ">
+        <div className="left flex gap-6 ">
           <h1 className="font-semibold">Tags</h1>
           <h1>Articles</h1>
           <h1>Guides</h1>
