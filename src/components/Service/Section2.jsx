053106cedfa2472d499bbf9fd7165b62a06bc697
Rajeev Kumar <46224203+rkcoola792@users.noreply.github.com>
2024-04-02 08:50:43 +0530

    service descritpion done

--- src/components/Service/Section2.jsx
+++ src/components/Service/Section2.jsx
@@ -5,20 +5,31 @@ import { StarIcon } from '../StarIcon/StarIcon';
 const Section2 = () => {
   return (
     <div className="section2  py-12 my-12   ">
-    <div className='right-12 absolute'>
-
-    <StarIcon></StarIcon>
-    </div>
+      <div className="right-6 absolute w-12">
+        <StarIcon></StarIcon>
+      </div>
       <div className="top-heading">
         <Heading
           heading="description"
           subheading="Service description"
         ></Heading>
       </div>
-      <div className="bottom flex my-12 ">
-        <div className="left w-[50%] flex flex-col gap-4">
+      <div className="bottom sm:flex my-12 ">
+        <div className="left sm:w-[50%] flex flex-col gap-4 ">
           <div className="left-subheadings flex gap-2">
-            <div className="mt-1">
+            <div className="mt-1 ml-2">
+              <img src="/tick-circle.png"></img>
+            </div>
+            <div></div>
+            <h1 className="w-[85%] text-[#555555] ">
+              Subodh Bajpai believes in a personalized approach. He and his team
+              take the time to understand the unique requirements and
+              aspirations of your business before recommending the most suitable
+              loan options. 
+            </h1>
+          </div>
+          <div className="left-subheadings flex gap-2">
+            <div className="mt-1 ml-2">
               <img src="/tick-circle.png"></img>
             </div>
             <h1 className="w-[85%] text-[#555555] ">
@@ -29,7 +40,7 @@ const Section2 = () => {
             </h1>
           </div>
           <div className="left-subheadings flex gap-2">
-            <div className="mt-1">
+            <div className="mt-1 ml-2">
               <img src="/tick-circle.png"></img>
             </div>
             <h1 className="w-[85%] text-[#555555] ">
@@ -40,18 +51,7 @@ const Section2 = () => {
             </h1>
           </div>
           <div className="left-subheadings flex gap-2">
-            <div className="mt-1">
-              <img src="/tick-circle.png"></img>
-            </div>
-            <h1 className="w-[85%] text-[#555555] ">
-              Subodh Bajpai believes in a personalized approach. He and his team
-              take the time to understand the unique requirements and
-              aspirations of your business before recommending the most suitable
-              loan options. 
-            </h1>
-          </div>
-          <div className="left-subheadings flex gap-2">
-            <div className="mt-1">
+            <div className="mt-1 ml-2">
               <img src="/tick-circle.png"></img>
             </div>
             <h1 className="w-[85%] text-[#555555] ">
@@ -62,7 +62,7 @@ const Section2 = () => {
             </h1>
           </div>
         </div>
-        <div className="right w-[50%]">
+        <div className="right sm:w-[50%] mt-6 sm:mt-0">
           <img src="/image 38.png" alt="" />
         </div>
       </div>
