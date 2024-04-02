dcf4f5ca8c94f26c0411d68a6132895a22279a64
Rajeev Kumar <46224203+rkcoola792@users.noreply.github.com>
2024-04-02 09:06:58 +0530

    have a project part done

--- src/components/Service/Section3.jsx
+++ src/components/Service/Section3.jsx
@@ -8,15 +8,16 @@ const Section3 = () => {
           src="/Call to Action.png
         "
           alt=""
+          className='h-[320px] sm:h-auto'
         />
-        <div className="texts absolute flex top-8 left-32 ">
-          <div className="left flex flex-col gap-8 w-[40%]">
+        <div className="texts absolute sm:flex-row flex flex-col  justify-between  top-8 sm:px-32 items-center">
+          <div className="left flex flex-col gap-8 sm:w-[60%]">
             <div className="heading">
-              <h1 className="text-white font-semibold text-[36px]">
+              <h1 className="text-white font-semibold sm:text-[36px] text-2xl text-center sm:text-left sm:leading-[40px]">
                 Have a project in mind! Let’s work together.
               </h1>
             </div>
-            <div className="subheading text-[16px] text-white opacity-90">
+            <div className="subheading text-[16px] text-white opacity-90 w-[90%] text-center sm:text-left ml-4 sm:ml-0">
               <h1>
                 Nam tincidunt condimentum arcu, rhoncus interdum eros vulputate
                 ut. Maecenas molestie sodales tristique. Nunc scelerisque tortor
@@ -24,7 +25,7 @@ const Section3 = () => {
               </h1>
             </div>
           </div>
-          <div className="right absolute right-24 top-24">
+          <div className="right  py-6">
             <div className="contact-us-button bg-white text-mainBlue p-2 px-6 py-3 font-semibold rounded-full cursor-pointer ">
               Contact Me
             </div>
