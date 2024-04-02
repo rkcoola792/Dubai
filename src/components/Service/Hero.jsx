a9923dc5d221bc500e17e5411093342732fb97de
Rajeev Kumar <46224203+rkcoola792@users.noreply.github.com>
2024-04-02 12:39:28 +0530

    service hero all responsive

--- src/components/Service/Hero.jsx
+++ src/components/Service/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = ({img,heading1,heading2,subheading,description}) => {
   return (
     <div className="hero-section rounded-lg sm:my-12 my-6">
       <div className="background-image flex flex-col gap-8 items-center text-center relative">
-        <img src={`${img ? img : "service.png"}`} className="z-10 h-[450px] sm:h-auto object-cover rounded-lg my-6 sm:my-0"></img>
+        <img src={`${img ? img : "service.png"}`} className="z-10 h-[450px]  object-cover rounded-lg my-6 sm:my-0"></img>
         <div className="headings z-20 absolute sm:w-[70%] top-[25%] flex flex-col gap-8 justify-center items-center">
           <h1 className="capitalize text-white text-2xl flex items-center gap-2 font-semibold ">
             {heading1}
