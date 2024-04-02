10ee6b5516def1bc8d6ddb25c0af5db91e42e18c
Rajeev Kumar <46224203+rkcoola792@users.noreply.github.com>
2024-04-02 16:18:02 +0530

    blogs section respoive

--- src/components/Blogs/MutipleBlogs.jsx
+++ src/components/Blogs/MutipleBlogs.jsx
@@ -3,7 +3,7 @@ import Singleblog from './Singleblog'
 
 const MutipleBlogs = () => {
   return (
-    <div className="my-12 sm:flex-row justify-between flex flex-col gap-12">
+    <div className="my-12 md:flex-row justify-center items-center flex flex-col gap-12">
       <Singleblog
         heading="Organize your digital assets  with a new methodology."
         subheading="In today's digital age, managing and organizing an ever-expanding array of digital assets can be a daunting task."
