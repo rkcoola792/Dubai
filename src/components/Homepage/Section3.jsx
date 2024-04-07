0ba56ebea305b107a3238212bd04e1c299773f78
rkcoola792 <46224203+rkcoola792@users.noreply.github.com>
2024-04-07 12:17:24 +0530

    section 3 mobile done

--- src/components/Homepage/Section3.jsx
+++ src/components/Homepage/Section3.jsx
@@ -34,17 +34,17 @@ const Section3 = () => {
       </motion.div>
       <div className="flex flex-col gap-24 mt-12  px-4 sm:px-12 lg:px-12 2xl:px-0  " ref={ref}>
         
-        <div className="group1 flex flex-col sm:gap-12 sm:sticky  sm:top-40 bg-white z-10 shadow-lg border border-gray-100  p-12  rounded-lg">
+        <div className="group1 sticky top-20 flex flex-col sm:gap-12 sm:sticky  sm:top-40 bg-white z-10 shadow-lg border border-gray-100 p-4  sm:p-12  rounded-lg">
           <motion.div
-            className="heading flex gap-6 items-center mb-4 sm:mb-0  "
+            className="heading flex gap-6 items-center justify-center sm:justify-normal mb-4 sm:mb-0  "
             initial={{ y: 75, opacity: 0 }}
             whileInView={{ y: 0, opacity: 1 }}
             transition={{ duration: 0.5, delay: 0.1 }}
           >
-            <div className="mt-1 ">
+            <div className="mt-1 hidden sm:block">
               <SmallStar></SmallStar>
             </div>
-            <h3 className="sm:text-2xl lg:text-3xl text-xl font-semibold">Quick Loans</h3>
+            <h3 className="sm:text-2xl lg:text-3xl text-xl font-semibold ">Quick Loans</h3>
           </motion.div>
           <Specialities
             subtitle1="Ability to raise funds through private funding on an urgent basis"
@@ -52,17 +52,17 @@ const Section3 = () => {
             subtitle3="Range of funding from Rs 5 lakhs to Rs 50 crores"
           />
         </div>
-        <div className="group2 flex flex-col sm:gap-12 sm:sticky  sm:top-40 bg-white z-20 shadow-lg border border-gray-100  p-12  rounded-lg">
+        <div className="group2 flex  sticky top-20  flex-col sm:gap-12 sm:sticky  sm:top-40 bg-white z-20 shadow-lg border border-gray-100 p-4 sm:p-12  rounded-lg">
           <motion.div
-            className="heading flex gap-2 items-center  "
+            className="heading flex gap-6 sm:gap-6 mb-4 items-center justify-center sm:justify-normal "
             initial={{ y: 75, opacity: 0 }}
             whileInView={{ y: 0, opacity: 1 }}
             transition={{ duration: 0.5, delay: 0.1 }}
           >
-            <div className="-mt-6 sm:mt-0">
+            <div className="hidden sm:block sm:mt-0">
               <SmallStar></SmallStar>
             </div>
-            <h3 className="sm:text-2xl lg:text-3xl text-xl font-semibold">
+            <h3 className="sm:text-2xl lg:text-3xl text-xl text-center sm:text-left font-semibold">
               PR & Communication Specialist
             </h3>
           </motion.div>
@@ -73,17 +73,17 @@ const Section3 = () => {
             subtitle3="Supporting NBFCs in raising funds for contingency"
           />
         </div>
-        <div className="group3 flex flex-col sm:gap-12 sm:sticky  sm:top-40 bg-white z-30 shadow-lg border border-gray-100  p-12  rounded-lg">
+        <div className="group3  sticky top-20  flex flex-col sm:gap-12 sm:sticky  sm:top-40 bg-white z-30 shadow-lg border border-gray-100 p-4  sm:p-12  rounded-lg">
           <motion.div
-            className="heading flex gap-2 items-center "
+            className="heading flex gap-6 sm:gap-6 mb-4 items-center justify-center text-center sm:justify-normal "
             initial={{ y: 75, opacity: 0 }}
             whileInView={{ y: 0, opacity: 1 }}
             transition={{ duration: 0.5, delay: 0.1 }}
           >
-            <div className="-mt-6 sm:mt-0">
+            <div className="hidden sm:block -mt-6 sm:mt-0">
               <SmallStar></SmallStar>
             </div>
-            <h3 className="sm:text-2xl lg:text-3xl text-xl font-semibold">
+            <h3 className="sm:text-2xl mb-4 sm:mb-0 lg:text-3xl text-xl font-semibold">
               Promoting Grassroot Entrepreneurship
             </h3>
           </motion.div>
--- src/components/Homepage/Specialities.jsx
+++ src/components/Homepage/Specialities.jsx
@@ -43,9 +43,9 @@ const Card = ({  subtitle, Icon, href }) => {
       <div className="absolute inset-0 bg-white translate-y-[100%] group-hover:translate-y-[0%] transition-transform duration-300" />
 
       {/* <Icon className="absolute z-10 -top-12 -right-12 text-9xl text-slate-100 opacity-90 group-hover:text-mainBlue group-hover:rotate-12 transition-transform duration-300" /> */}
-      <Icon className="mb-2 text-lg sm:text-xl md:text-2xl lg:text-3xl text-mainBlue  transition-colors relative z-10 duration-300" />
+      <Icon className="mb-2 text-4xl sm:text-xl md:text-2xl lg:text-3xl text-mainBlue  transition-colors relative z-10 duration-300" />
 
-      <p className="opacity-90  font-semibold  relative z-10 duration-300 lg:leading-7 leading-6 text-xs lg:text-sm xl:leading-8  2xl:text-xl 2xl:leading-8">
+      <p className="opacity-90  font-semibold  relative z-10 duration-300 lg:leading-7  text-base leading-8 lg:text-sm xl:leading-8  2xl:text-xl 2xl:leading-8">
         {subtitle}
       </p>
     </motion.div>
